45f2ce4d36daefb1d9ddd9e95840e3469b9a40f1
AndreasTveitdalG <124246474+AndreasTveitdalG@users.noreply.github.com>
2023-02-06 13:17:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ fetch('https://api.github.com/repos/AndreasTveitdalG/idk/contents/data.json')
   .then(responseData => {
     data = JSON.parse(atob(responseData.content));
 
-    let cardElement = `<div class="cards"><p><b>${data.entities.testfile1.title}</b><br>${data.entities.testfile1.description}<br><i>${data.entities.testfile1.class}</i></p></div>`;
+    let cardElement = `<div class="card"><p><b>${data.entities.testfile1.title}</b><br>${data.entities.testfile1.description}<br><i>${data.entities.testfile1.class}</i></p></div>`;
 
     const element = $(".test").append(cardElement);
   });
