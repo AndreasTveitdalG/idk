7d2507e5d35568f26fc8475db2305f31ed3f2602
AndreasTveitdalG <124246474+AndreasTveitdalG@users.noreply.github.com>
2023-02-06 08:58:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let data;
 
-fetch('github.com/AndreasTveitdalG/idk/data.json')
+fetch('https://github.com/AndreasTveitdalG/idk/blob/main/data.json')
   .then(response => response.json())
   .then(d => data = d);
 
