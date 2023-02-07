541222cd77c417f9b670907568ff277bfa5fc885
AndreasTveitdalG <124246474+AndreasTveitdalG@users.noreply.github.com>
2023-02-07 13:32:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ fetch('https://api.github.com/repos/AndreasTveitdalG/idk/contents/data.json')
     let cardElement = `
       <div class="card">
         <p>
-        <span class="cardTitle">${data.entities.testfile1.title}</span>
-        <span class="cardClass">${data.entities.testfile1.class}</span><br>
-        <span class="cardDescription">${data.entities.testfile1.description}</span>
+          <span class="cardTitle">${data.entities.testfile1.title}</span>
+          <span class="cardClass">${data.entities.testfile1.class}</span><br>
+          <span class="cardDescription">${data.entities.testfile1.description}</span>
        </p></div>`;
 
     const element = $(".cards").append(cardElement);
