aa4452ca218740b46964a69dcb48f706c49946ea
AndreasTveitdalG <124246474+AndreasTveitdalG@users.noreply.github.com>
2023-02-08 09:58:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ fetch('https://api.github.com/repos/AndreasTveitdalG/idk/contents/data.json')
         <div class="card">
           <p>
             <span class="cardTitle">
-              <a href="${data.entities["file" + fileIndex].link}">
+              <a href="${data.entities["file" + fileIndex].link}" target="_blank">
                 ${data.entities["file" + fileIndex].title}
             </a></span>
             <span class="cardClass">${data.entities["file" + fileIndex].class}</span><br>
