285d4678c766728bb4d545d6373e326b3cbbb1ca
AndreasTveitdalG <124246474+AndreasTveitdalG@users.noreply.github.com>
2023-02-08 09:25:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 let data;
-let cardElement;
+let cardElements = "";
 
 fetch('https://api.github.com/repos/AndreasTveitdalG/idk/contents/data.json')
   .then(response => response.json())
   .then(responseData => {
     data = JSON.parse(atob(responseData.content));
     
-    for (let fileIndex = 0; fileIndex > Object.keys(data.entities).length; fileIndex++) {
-      cardElement = `
+    for (let fileIndex = 0; fileIndex < Object.keys(data.entities).length; fileIndex++) {
+      cardElements += `
         <div class="card">
           <p>
             <span class="cardTitle">
-              <a href="${data.entities.file${fileIndex}.link}">
-                ${data.entities.file${fileIndex}.title}
+              <a href="${data.entities["file" + fileIndex].link}">
+                ${data.entities["file" + fileIndex].title}
             </a></span>
-            <span class="cardClass">${data.entities.file${fileIndex}.class}</span><br>
-            <span class="cardDescription">${data.entities.file${fileIndex}.description}</span>
-      </p></div>`;
+            <span class="cardClass">${data.entities["file" + fileIndex].class}</span><br>
+            <span class="cardDescription">${data.entities["file" + fileIndex].description}</span>
+          </p>
+        </div>`;
     }
-    const element = $(".cards").append(cardElement);
+    const element = $(".cards").append(cardElements);
 });
